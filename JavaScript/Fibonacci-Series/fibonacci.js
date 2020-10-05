06e2f77cea227e513cff6a3eeec9ce9d27a119e4
JudithMishel <frontend2@medialabla.com>
2020-10-04 19:52:00 -0500

    Add fibonacci algorithm

---
+++ JavaScript/Fibonacci-Series/fibonacci.js
@@ -0,0 +1,17 @@
+//Function that allows showing the Fibonacci series where each term is the sum of the previous two. 
+//The function will show elements according to the parameter set in it.
+// Author: @mishelpa
+
+function fibonacci(number){
+
+    let fiboArray = [0, 1];
+    
+    for(i = 2; i < number;i++){
+     fiboArray.push(fiboArray[i-1] + fiboArray[i-2]);
+    }
+
+    return fiboArray;
+  }
+
+  
+  
